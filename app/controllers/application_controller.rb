76e885ffd3b1eee4504f2dfc3272b71bafd5e94b
taylorkraft <taylorkraft16@gmail.com>
2020-01-16 10:26:59 -0700

    Initial

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get '/' do
+    "<h1>Welcome to Brewed!</h1>"
+  end
+
+  
+end
